2b8d3d376bb5ba874ca0cd3aa746b72a6f17d14b
mike <wamgguoping@aipai.com>
2016-03-10 14:35:06 +0800

    1、添加图片资源和第三方库

---
+++ 小日子/小日子/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  小日子
+//
+//  Created by mike on 16/3/10.
+//  Copyright © 2016年 MK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
